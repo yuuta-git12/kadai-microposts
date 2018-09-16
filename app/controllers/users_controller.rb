a67d80500240e1cf771ff31582b619f811955ad7
yuuta.git12 <tayu2201@icloud.com>
2018-09-16 17:17:13 +0000

    timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-   before_action :require_user_logged_in, only: [:index, :show :followings, :followers]
+   before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
    
   def index
     @users = User.all.page(params[:page])
@@ -38,9 +38,6 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
-  
-  
-  
   
   
   private
